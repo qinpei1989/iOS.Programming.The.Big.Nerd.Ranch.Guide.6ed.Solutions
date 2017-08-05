b9684d53b1d2640069d99c7372f21ea1207b9c57
Pei Qin <qinpei1989@gmail.com>
2017-08-05 15:09:36 +0800

    Add solution to ch10: gold challenge

--- 10 - TableViewControllers/Solutions/Homepwner/Homepwner/ItemsViewController.swift
+++ 10 - TableViewControllers/Solutions/Homepwner/Homepwner/ItemsViewController.swift
@@ -17,6 +17,8 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.backgroundView = UIImageView(image: UIImage(named: "Apple"))
     }
     
     
@@ -34,6 +36,7 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Create an instance of UITableViewCell, with default appearance
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.backgroundColor = UIColor.clear
         
         /* The last row of the last section should display "No more items!" */
         if indexPath.section == 1 && indexPath.row == itemStore.cheapItems.count {
@@ -49,7 +52,9 @@ class ItemsViewController: UITableViewController {
             }
             
             cell.textLabel?.text = item.name
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
             cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 20)
         }
         
         return cell
@@ -61,12 +66,36 @@ class ItemsViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+    /*
+     * Gold Challenge: Customizing the Table
+     *
+     * If tableView(_:titleForHeaderInSection:) is used, headers will block the background image
+     */
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 30))
+        let textLabel = UILabel(frame: CGRect(x: 20, y: 0, width: tableView.frame.width - 50, height: 30))
+        
         if section == 0 {
-            return "> $50"
+            textLabel.text = "> $50"
         } else {
-            return "<= $50"
+            textLabel.text = "<= $50"
+        }
+        
+        headerView.addSubview(textLabel)
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == itemStore.cheapItems.count {
+            return 44
+        } else {
+            return 60
         }
     }
 }
